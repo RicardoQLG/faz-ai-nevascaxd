cbcb6ef6c964beae291b3970bb2dba2520a1a92b
Ricardo Godoy <rq.godoy@gmail.com>
2022-05-25 22:53:16 -0300

    feat: ensure AskForItems call SendToChat with correct params

--- src/domain/useCases/AskForItems.ts
+++ src/domain/useCases/AskForItems.ts
@@ -1,12 +1,17 @@
 import { GetAllItemRepository } from '../protocols/GetAllItemRepository'
+import { SendToChat } from '../protocols/SendToChat'
 
 export class AskForItems {
   private readonly getAllItemRepository: GetAllItemRepository
-  constructor (getAllItemRepository: GetAllItemRepository) {
+  private readonly sendToChatStub: SendToChat
+  constructor (getAllItemRepository: GetAllItemRepository, sendToChatStub: SendToChat) {
     this.getAllItemRepository = getAllItemRepository
+    this.sendToChatStub = sendToChatStub
   }
 
   async handle (): Promise<void> {
-    await this.getAllItemRepository.get()
+    const items = await this.getAllItemRepository.get()
+    const promises = items.map(async item => await this.sendToChatStub.send(item))
+    await Promise.all(promises)
   }
 }
