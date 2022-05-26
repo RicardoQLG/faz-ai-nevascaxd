4f06d2de0aff9ff9e6c9def0973b8b8c12a161ab
Ricardo Godoy <rq.godoy@gmail.com>
2022-05-25 22:53:23 -0300

    test: ensure AskForItems call SendToChat with correct params

--- src/domain/useCases/AskForItems.test.ts
+++ src/domain/useCases/AskForItems.test.ts
@@ -1,5 +1,10 @@
 import { GetAllItemRepository } from '../protocols/GetAllItemRepository'
+import { SendToChat } from '../protocols/SendToChat'
 import { AskForItems } from './AskForItems'
+
+class SendToChatStub implements SendToChat {
+  async send (message: string): Promise<void> {}
+}
 
 class GetAllItemRepositoryStub implements GetAllItemRepository {
   async get (): Promise<any> {
@@ -8,14 +13,17 @@ class GetAllItemRepositoryStub implements GetAllItemRepository {
 }
 
 interface SutTypes {
+  sendToChatStub: SendToChatStub
   getAllItemRepositoryStub: GetAllItemRepositoryStub
   sut: AskForItems
 }
 
 const makeSut = (): SutTypes => {
+  const sendToChatStub = new SendToChatStub()
   const getAllItemsStub = new GetAllItemRepositoryStub()
-  const sut = new AskForItems(getAllItemsStub)
+  const sut = new AskForItems(getAllItemsStub, sendToChatStub)
   return {
+    sendToChatStub,
     getAllItemRepositoryStub: getAllItemsStub,
     sut
   }
@@ -27,5 +35,12 @@ describe('AskForItems', () => {
     const getSpy = jest.spyOn(getAllItemRepositoryStub, 'get')
     await sut.handle()
     expect(getSpy).toBeCalledTimes(1)
+  })
+
+  test('should call SendToChat with correct params', async () => {
+    const { sut, sendToChatStub } = makeSut()
+    const sendSpy = jest.spyOn(sendToChatStub, 'send')
+    await sut.handle()
+    expect(sendSpy).toBeCalledWith('valid_task')
   })
 })
